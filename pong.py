e414a659cfc170269b11c0caf0e463e79f96d16e
Thibault Vacheron <thib.du-42@hotmail.fr>
2018-12-12 16:32:47 +0100

    balle + paddle

--- pong.py
+++ pong.py
@@ -12,6 +12,18 @@ y = 5
 x_sens = 1
 y_sens = 1
 
+
+
+paddle_y = 1
+paddle_x = 1
+
+x2 = 0
+y2 = 1
+
+
+
+
+
 def balle():
   global x, y, x_sens, y_sens
   x = x + x_sens
@@ -28,12 +40,41 @@ def balle():
   elif y == 0:
     y_sens = 1
     
-          
+
+
+def update_screen():
+    
+    sense.set_pixel(x2, y2, 255, 255, 255)
+    sense.set_pixel(x2, y2+1, 255, 255, 255)
+    sense.set_pixel(x2, y2+2, 255, 255, 255)
+
+def clamp(value, min_value=0, max_value=5):
+    return min(max_value, max(min_value, value))
+
+def move_dot(event):
+    global x2, y2
+    if event.action in ('pressed', 'held'):
+
+        y2 = clamp(y2 + {
+            'up': -1,
+            'down': 1,
+            }.get(event.direction, 0))
+
+
+
+  
         
           
   
 while True:
+  
+
   sense.clear()
+  update_screen()
+  sense.stick.direction_up = move_dot
+  sense.stick.direction_down = move_dot
+  sense.stick.direction_any = update_screen
   balle()
   sleep(0.10)
+  
  
