c08f1132aa449f89ab2bd3945c91a60909838554
Thibault Vacheron <thib.du-42@hotmail.fr>
2018-12-12 11:26:34 +0100

    la balle rebondi

--- pong.py
+++ pong.py
@@ -1,7 +1,37 @@
 from sense_hat import SenseHat
 
+from time import sleep
+
 sense=SenseHat()
 
 sense.clear()
 
-sense.set_pixel(0, 0, 255, 0, 0)
+x = 0
+
+y = 0
+
+while x >0 and y >0 or x < 7 and y < 7:
+
+  while x < 7 and y < 7 :
+    x = x + 1
+    y = y + 1
+  
+ 
+  
+  
+    sense.clear()
+    sense.set_pixel(x, y, 255, 0, 0)
+    sleep(0.10)
+  
+  
+
+  while x >0 and y >0:
+    x = x - 1
+    y = y - 1
+  
+ 
+  
+  
+    sense.clear()
+    sense.set_pixel(x, y, 255, 0, 0)
+    sleep(0.10)
